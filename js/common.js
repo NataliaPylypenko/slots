3e1bc92f95d08774530d512ed92a1c769356a6e9
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-12-18 16:17:53 +0200

    language versions

---
+++ js/common.js
@@ -0,0 +1,77 @@
+const messages = {
+    'en': {
+        "playNow": "Play now",
+    },
+    'pt': {
+        "playNow": "Jogue agora",
+    },
+    'de': {
+        "playNow": "Jetzt spielen",
+    },
+    'pl': {
+        "playNow": "Zagraj teraz",
+    },
+    'ca-en': {
+        "playNow": "Play now",
+    },
+    'ca-fr': {
+        "playNow": "Jouer nouveau",
+    },
+    'au': {
+        "playNow": "Play now",
+    },
+    'nz': {
+        "playNow": "Play now",
+    },
+    'in': {
+        "playNow": "Play now",
+    },
+};
+
+const refs = {
+    userLanguage: navigator.language || navigator.userLanguage,
+    url: new URL(window.location.href),
+    playNow: document.querySelector('#playNow'),
+    selectLanguage: document.querySelector('.languagepicker-dropduwn')
+};
+
+// languagepicker
+function detectLanguage() {
+    const paramName = 'lang';
+    if (!refs.url.searchParams.has(paramName)) {
+        refs.url.searchParams.set(paramName, refs.userLanguage);
+        window.history.replaceState({}, '', refs.url);
+        window.location.reload();
+    }
+}
+
+function getLanguage() {
+    return refs.url.searchParams.get('lang');
+}
+const currentLanguage = getLanguage();
+
+function setLanguage(language) {
+    const paramName = 'lang';
+    refs.url.searchParams.set(paramName, language);
+    window.history.replaceState({}, '', refs.url);
+    window.location.reload();
+}
+
+function changeLanguageHandler() {
+    refs.selectLanguage.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (e.target.tagName === 'A') {
+            const language = e.target.dataset.lang;
+            setLanguage(language);
+        }
+    });
+
+    const img = document.querySelector('.languagepicker a img');
+    img.src = `images/lang/${currentLanguage}.png`;
+}
+
+detectLanguage();
+changeLanguageHandler();
+
+// rendering of texts
+refs.playNow.innerHTML = messages[currentLanguage]?.playNow || 'Play now';
